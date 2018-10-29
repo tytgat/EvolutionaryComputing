d5f38279d63d44c6d4c5073be3480c5f62b11e4e
Tytgat Karel <tytgatkarel@gmail.com>
2018-10-29 09:05:41 +0300

    1st Commit TP2

---
+++ TP2/EC_lab2-master/src/main/java/MyCrossover.java
@@ -0,0 +1,21 @@
+import org.uncommons.watchmaker.framework.operators.AbstractCrossover;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MyCrossover extends AbstractCrossover<double[]> {
+    protected MyCrossover() {
+        super(1);
+    }
+
+    protected List<double[]> mate(double[] p1, double[] p2, int i, Random random) {
+        ArrayList children = new ArrayList();
+
+        // your implementation:
+
+//        children.add(p1);
+//        children.add(p2);
+        return children;
+    }
+}
---
+++ TP2/EC_lab2-master/src/main/java/MyMutation.java
@@ -0,0 +1,16 @@
+import org.uncommons.watchmaker.framework.EvolutionaryOperator;
+
+import java.util.List;
+import java.util.Random;
+
+public class MyMutation implements EvolutionaryOperator<double[]> {
+    public List<double[]> apply(List<double[]> population, Random random) {
+        // initial population
+        // need to change individuals, but not their number!
+
+        // your implementation:
+
+        //result population
+        return population;
+    }
+}
